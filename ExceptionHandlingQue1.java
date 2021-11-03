928601bec82343aa510b07ecf8663cec0c9eec23
Anuj-kiet <53224310+Anuj-kiet@users.noreply.github.com>
2021-11-03 13:54:15 +0530

    Add files via upload

---
+++ ExceptionHandlingQue1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ExceptionHandlingQue1{
+	public static void main(String[] args) {
+			System.out.println("Exception program start");
+			Scanner sc=new Scanner(System.in);
+			System.out.println("Enter the first number:");
+			int a=sc.nextInt();
+			System.out.println("Enter the second number:");
+			int b=sc.nextInt();
+			try {
+			int result=a/b;
+			System.out.println("Result is : " +result);
+		} catch (ArithmeticException e) {
+			e.printStackTrace();
+			System.out.println("After dividing by integer 0 :" +e);
+		}
+			if(b==0)
+			{
+				System.out.println("Exception Handled");
+			}
+		   
+	}
+}
